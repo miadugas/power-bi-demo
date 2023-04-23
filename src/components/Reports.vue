20637072907f4007251a47ad0c32c94ef5fbad33
Mia Dugas <miadugas@Mias-MacBook-Air.local>
2023-04-23 09:30:30 -0600

    test render

--- src/components/Reports.vue
+++ src/components/Reports.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="mb-20 relative" style="padding-bottom: 47.5%; height: 0;">
-  <iframe
+  <!-- <iframe
     title="Sample Report Demo"
     class="absolute top-0 left-0 w-full h-full"
     src="https://playground.powerbi.com/sampleReportEmbed"
     frameborder="0"
-    allowFullScreen="true"></iframe>
+    allowFullScreen="true"></iframe> -->
 </div>
 
 </template>
